73b4ada86de8f64386e59e9b5447315c9944b544
JoseMariaEspinoza <josemaria.espinoza.adm@gmail.com>
2025-09-26 10:17:03 +0200

    EjerciciosIfElse.java

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-         /*Los tipos Primitivos en JAVA
-         En JAVA existen 8 tipos primitivos que se podrian clasificar en los de numero entero:
-         byte, short, int y long (ordenados del más corto al más largo), los de numeros decimal o de coma flotante:
-         float y double (ordenados tambien de menor capacidad a mayor), los caracteres: char,
-         y por ultimo los valores lógicos: boolean, a continación declararé e iniciaré uno de cada tipo
-          */
-        //byte puede contener valores entre -128 por debajo y 127 por arriba
-        byte bMenor = -128, bMayor = 127;
-        //short puede contener valores entre -32768 por debajo y 32767 por arriba
-        short sMenor = -32768, eMayor= 32767;
-        //int puede contener valores entre -2147483648 por debajo y 2147483647 por arriba
-        int iMenor = -2147483648, iMayor = 2147483647;
-        //long puede contener valores entre -9223372036854775808 por arriba y 9223372036854775807 por debajo
-        long lMenor = -9223372036854775808L, lMayor = 9223372036854775807L;
-        //Al instanciar un float podemos poner tantos decimales como queramos pero a la hora de operar con el, el sistema soolo tomara un maximo de 7, por eso es más preciso usar double
-        float decimalPequenio = 0.1234567F; double decimalGrande = 0.123456789;
-        //char acepta  un caracter del tipo que sea, inclusive numerico
-        char a='a', b='1', c='*', d='.';
-        //Y por último está boolean que solo puede contener el valor verdadero o falso
-        boolean bVerdadero= true, bFalso= false;
+package ejerciciosbasicos;
 
+import java.util.Scanner;
+
+public class EjerciciosIfElse {
+    public static void main(String[] args) {
+//    1. Escribir un programa, de nombre mayorDeEdad, que solicite la edad como entero y muestre
+//        un mensaje por pantalla si es mayor de edad o no.
+        int edad;
+        System.out.println("Introduzca su edad");    
+        Scanner sc=new Scanner(System.in);
+        edad=sc.nextInt();
+        if(edad>=18){
+            System.out.println("Si es mayor de edad");
+        }else{
+            System.out.println("No es mayor de edad");
+            }
+//    2. Escribir un programa, de nombre mayorQue25, que pida entero al usuario y muestre un
+//        mensaje por pantalla que indique si es mayor o menor que 25.
+        System.out.println("Introduce un número entero");
+        edad=sc.nextInt();
+        if(edad>25){
+            System.out.println("Es mayor de 25 años");
+        }else{
+            System.out.println("Esmenor de 25 años");
+        }
     }
+    
 }
